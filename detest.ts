a4d4625a4b559c864da2c2c030c1688dcf479a76
ncphillips <ncphillips.19@gmail.com>
2018-09-14 19:21:31 -0300

    extracted context logger and test runner

--- detest.ts
+++ detest.ts
@@ -1,60 +1,7 @@
 import { Test } from "./src/test.ts"
-import { Context } from "./src/context.ts"
+export { Context } from "./src/context.ts"
+export { TestRunner } from "./src/test-runner.ts"
 
-export { Context } 
-
-export class TestRunner {
-  logger: ContextLogger = new ContextLogger()
-  runTests(context: Context) {
-    this.logger.context = context
-    this.logger.logDescription()
-
-    context.tests.forEach((test: Test) => {
-      try {
-        test.run()
-        this.logger.logPassingTest(test)
-      } catch (e) {
-        this.logger.logFailingTest(test)
-      }
-    })
-
-    context.contexts.map(nestedContext => {
-      this.runTests(nestedContext)
-    })
-  }
-}
-
-class ContextLogger {
-  public context: Context | null = null
- 
-  logDescription() {
-    this.log(this.context.description)
-  }
-
-  logPassingTest(test: Test) {
-    this.log(`  - ${test.description} (PASS)`)
-  }
-
-  logFailingTest(test: Test) {
-    this.log(`  - ${test.description} (FAIL)`)
-  }
- 
-  private get nestingLevel() {
-    if (!this.context) return 0
-    return this.context.nestingLevel
-  }
-
-  private get tabs(): string {
-    let t = ""
-    for (let i = 0; i < this.context.nestingLevel; i++) t += "  "
-    return t
-  }
-
-  private log(message: string) {
-    console.log(`${this.tabs}${message}`)
-  }
-
-}
 
 export function expect<T = any>(actual: T) {
   return new DeferredExpectation<T>(actual)
