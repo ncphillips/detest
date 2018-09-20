20ce6cf065cda95869dc0eca399bfdbc068f3474
ncphillips <ncphillips.19@gmail.com>
2018-09-20 18:46:32 -0300

    TestingFramework is now the DescribeItApi

--- detest.ts
+++ detest.ts
@@ -1,8 +1,8 @@
-import { TestingFramework } from "./src/testing-framework.ts"
+import { DescribeItApi } from "./src/describe-it-api.ts"
 import { ContextLogger } from "./src/context-logger.ts"
 
 let listeners = [new ContextLogger()]
-let { describe, it, before, beforeEach, test, runTests } = new TestingFramework(listeners)
+let { describe, it, before, beforeEach, test, runTests } = new DescribeItApi(listeners)
 
 export { describe, it, test, before, beforeEach, runTests }
 export { expect } from "./src/expect.ts"
